083b5287bd49ff2cf260318faf45003c9dcb8e37
MManiSSingh <166468945+MManiSSingh@users.noreply.github.com>
2025-02-25 21:05:53 -0800

    Update map.js

--- map.js
+++ map.js
@@ -10,11 +10,11 @@ const map = new mapboxgl.Map({
 });
 
 map.on('load', async () => {
-
   map.addSource('boston_route', {
     type: 'geojson',
     data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson'
   });
+
   map.addLayer({
     id: 'bike-lanes',
     type: 'line',
@@ -30,6 +30,7 @@ map.on('load', async () => {
     type: 'geojson',
     data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson'
   });
+
   map.addLayer({
     id: 'cambridge-bike-lanes',
     type: 'line',
@@ -41,6 +42,9 @@ map.on('load', async () => {
     }
   });
 
+  const svg = d3.select('#map').select('svg');
+  let stations = [];
+
   const jsonurl = 'https://dsc106.com/labs/lab07/data/bluebikes-stations.json';
   d3.json(jsonurl).then(jsonData => {
     const stations = jsonData.data.stations;
@@ -49,4 +53,21 @@ map.on('load', async () => {
   }).catch(error => {
     console.error('Error loading JSON:', error); 
   });
+
+  map.on('move', updatePositions);    
+  map.on('zoom', updatePositions);   
+  map.on('resize', updatePositions);
+  map.on('moveend', updatePositions); 
+
+  function getCoords(station) {
+    const point = new mapboxgl.LngLat(+station.lon, +station.lat);
+    const { x, y } = map.project(point);
+    return { cx: x, cy: y }; }
+
+  function updatePositions() {
+    circles
+    .attr('cx', d => getCoords(d).cx)  
+    .attr('cy', d => getCoords(d).cy); 
+    }
+  
 });
