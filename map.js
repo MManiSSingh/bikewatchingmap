e90be24e6e73382b54899ed29b55ae397c104da9
MManiSSingh <166468945+MManiSSingh@users.noreply.github.com>
2025-02-25 20:02:18 -0800

    Update map.js

--- map.js
+++ map.js
@@ -10,3 +10,38 @@ const map = new mapboxgl.Map({
   minZoom: 5, // Minimum allowed zoom
   maxZoom: 18 // Maximum allowed zoom
 });
+
+map.on('load', () => {
+    map.addSource('boston_route', {
+      type: 'geojson',
+      data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson'
+    });
+  
+    map.addLayer({
+      id: 'bike-lanes',
+      type: 'line',
+      source: 'boston_route',
+      paint: {
+        'line-color': 'green',
+        'line-width': 3,
+        'line-opacity': 0.4
+      }
+    });
+  
+    map.addSource('cambridge_bike_lanes', {
+      type: 'geojson',
+      data: 'YOUR_CAMBRIDGE_GEOJSON_URL_HERE'
+    });
+  
+    map.addLayer({
+      id: 'cambridge-bike-lanes',
+      type: 'line',
+      source: 'cambridge_bike_lanes',
+      paint: {
+        'line-color': '#1E90FF',  
+        'line-width': 3,
+        'line-opacity': 0.5
+      }
+    });
+  });
+  
